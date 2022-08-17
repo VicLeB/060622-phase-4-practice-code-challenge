f3b8eb3740c027eaaa92adbe69e9cad2fd251d0d
Victoria LeBel <vickileb@hotmail.com>
2022-08-17 09:45:42 -0700

    deliverables completed

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,18 @@
 class AppearancesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def create
+        appearance = Appearance.create!(appearance_params)
+        render json: appearance, status: :created
+    end
+
+    private
+
+    def appearance_params
+        params.permit(:rating, :episode_id, :guest_id)
+    end
+
+    def render_unprocessable_entity(e)
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
